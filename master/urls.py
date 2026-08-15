cca1d89322e4dcfe31e876f97fe69e9fb298b641
BhushanArigela <32087680+BhushanArigela@users.noreply.github.com>
2026-08-15 14:19:17 +0530

    Update urls.py

--- master/urls.py
+++ master/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('sitemap/', SitemapView.as_view(), name='sitemap'),
     path('api/contact/submit/', submit_contact_form, name='api_contact_submit'),
     path('robots.txt', RobotsTxtView.as_view(), name='robots_txt'),
+    path('llms.txt', LLMTxtView.as_view(), name='llm_txt'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap_xml'),
     # Error Page Previews
     path('404/', HomeView.as_view(template_name='404.html'), name='preview_404'),
